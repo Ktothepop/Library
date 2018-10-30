c21ef81007a84ff1ae59fc1340f5aaf62d13a187
Patrik <Patrik.chu.4303@student.uu.se>
2018-10-30 14:08:45 +0100

    Dialog för att lägga till böcker almost done.

---
+++ Library/AddBookDialog.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Library
+{
+    public partial class AddBookDialog : Form
+    {
+        private string ISBN;
+        private string Title;
+        private string Description;
+        private string BookAuthor;
+
+        public string _ISBN
+        {
+            get { return ISBN; }
+            set { ISBN = value; }
+        }
+        public string _Title
+        {
+            get { return Title; }
+            set { Title = value; }
+        }
+        public string _Description
+        {
+            get { return Description; }
+            set { Description = value; }
+        }
+        public string _BookAuthor
+        {
+            get { return BookAuthor; }
+            set { BookAuthor = value; }
+        }
+        public AddBookDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAddBook_Click(object sender, EventArgs e)
+        {
+            _ISBN = textBoxISBN.Text;
+            _Title = textBoxTitle.Text;
+            _Description = textBoxDescription.Text;
+            _BookAuthor = textBoxAuthor.Text;
+        }
+    }
+}
--- Library/LibraryForm.cs
+++ Library/LibraryForm.cs
@@ -13,44 +13,58 @@ using System.Windows.Forms;
 
 namespace Library
 {
-  public partial class LibraryForm : Form
-  {
-
-    BookService bookService;
-
-    public LibraryForm()
-    {
-      InitializeComponent();
-
-      // we create only one context in our application, which gets shared among repositories
-      LibraryContext context = new LibraryContext();
-      // we use a factory object that will create the repositories as they are needed, it also makes
-      // sure all the repositories created use the same context.
-      RepositoryFactory repFactory = new RepositoryFactory(context);
-
-      this.bookService = new BookService(repFactory);
-
-      ShowAllBooks(bookService.All());
-    }
-
-
-    private void ShowAllBooks(IEnumerable<Book> books)
-    {
-      lbBooks.Items.Clear();
-      foreach (Book book in books) {
-        lbBooks.Items.Add(book.Title + " " + " [5]");
-      }
-    }
-
-    
-
-    
-
-    private void LibraryForm_Load(object sender, EventArgs e)
+    public partial class LibraryForm : Form
     {
 
-    }
+        BookService bookService;
 
- 
-  }
+        public LibraryForm()
+        {
+            InitializeComponent();
+
+            // we create only one context in our application, which gets shared among repositories
+            LibraryContext context = new LibraryContext();
+            // we use a factory object that will create the repositories as they are needed, it also makes
+            // sure all the repositories created use the same context.
+            RepositoryFactory repFactory = new RepositoryFactory(context);
+
+            this.bookService = new BookService(repFactory);
+
+            ShowAllBooks(bookService.All());
+        }
+
+
+        private void ShowAllBooks(IEnumerable<Book> books)
+        {
+            lbBooks.Items.Clear();
+            foreach (Book book in books)
+            {
+                lbBooks.Items.Add(book.Title + " " + " [5]");
+            }
+        }
+
+
+
+
+
+        private void LibraryForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Create_Book_Click(object sender, EventArgs e)
+        {
+            //Detta fungerar nu måste vi bara fundera på hur vi gerdom här värdena till en service.
+            //Även hur vi konverterar en string till en Author (_BookAuthor)
+            AddBookDialog abd = new AddBookDialog();
+            if (abd.ShowDialog() == DialogResult.OK)
+            {
+                Testbox.Items.Add(abd._ISBN);
+                Testbox.Items.Add(abd._Title);
+                Testbox.Items.Add(abd._BookAuthor);
+                Testbox.Items.Add(abd._Description);
+
+            }
+        }
+    }
 }
